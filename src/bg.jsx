ef153cb33e03622f4d30631e83dfa4b1ba3c6d71
cairq <cairq@tongbaninfo.com>
2025-06-24 00:10:36 +0800

    first commit

---
+++ src/bg.jsx
@@ -0,0 +1,51 @@
+import { useRef, useEffect } from 'react'
+
+import './style/bg.css'
+
+const createImage = (canvasDom) => {
+    const ctx = canvasDom.getContext("2d");
+    // 设置画布尺寸
+    canvasDom.width = document.querySelector('.App').getBoundingClientRect().width;
+    canvasDom.height = document.querySelector('.App').getBoundingClientRect().height;
+    // 生成噪点
+    const imageData = ctx.createImageData(canvasDom.width, canvasDom.height);
+    for (let i = 0; i < imageData.data.length; i += 4) {
+        const gray = Number(Math.random() * 255); // 随机灰度值
+        imageData.data[i] = gray;       // R
+        imageData.data[i + 1] = gray;   // G
+        imageData.data[i + 2] = gray;   // B
+        imageData.data[i + 3] = 25;     // A (透明度)
+    }
+    ctx.putImageData(imageData, 0, 0);
+}
+
+export default function Background(props) {
+    console.log(props)
+    const canvasDom = useRef(null)
+
+    useEffect(() => {
+        if (canvasDom.current) {
+            createImage(canvasDom.current)
+        }
+    }, [canvasDom])
+
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (canvasDom.current) {
+                createImage(canvasDom.current)
+            }
+        };
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return (
+        <div className='bg-box'>
+            <canvas ref={canvasDom}></canvas>
+            <div className='bg-box__child'>
+                {props.children}
+            </div>
+        </div>
+    )
+}
---
+++ src/components/info.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+
+const baseText = `Hello World!记录我的开发之路.如果你感兴趣欢迎留言.`
+
+
+export default function MyInfo() {
+    const [displayedText, setDisplayedText] = useState('');
+
+    useEffect(() => {
+        let animationFrame;
+        let lastTime = performance.now();
+        const delay = 80; // 每个字符的延迟，单位为毫秒
+        let indexRef = 0;
+
+        function typeWriter(now) {
+            if (indexRef <= baseText.length) {
+                if (now - lastTime >= delay) {
+                    setDisplayedText(baseText.slice(0, indexRef));
+                    indexRef += 1;
+                    lastTime = now;
+                }
+                animationFrame = requestAnimationFrame(typeWriter);
+            } else {
+                cancelAnimationFrame(animationFrame)
+            }
+        }
+        animationFrame = requestAnimationFrame(typeWriter);
+        return () => cancelAnimationFrame(animationFrame);
+    }, []);
+
+    return (
+        <div className="base-info">
+            <p>{displayedText}<span className="base-info__cursor-text"></span></p>
+        </div>
+    )
+}
